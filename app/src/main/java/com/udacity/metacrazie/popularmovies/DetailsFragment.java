39ed2a1f3793e5ccaa1b3eeff8b7dbd86e6ac707
MetaCrazie <pratyusha.taurus@gmail.com>
2016-05-15 14:59:29 +0530

    Fix internet connectivity issue

--- app/src/main/java/com/udacity/metacrazie/popularmovies/DetailsFragment.java
+++ app/src/main/java/com/udacity/metacrazie/popularmovies/DetailsFragment.java
@@ -4,11 +4,33 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class DetailsFragment extends Fragment {
+
+
+    int id;
+    String title;
+    String plot;
+    String date;
+    String poster;
+    String vote;
+    String bg;
+
+
+    ArrayList<ReviewObj> rList = new ArrayList<>();
+    RecyclerView tView;
+
+    ArrayList<TrailerObj> tList = new ArrayList<>();
+    RecyclerView rView;
+
+
+
 
     public DetailsFragment() {
         // Required empty public constructor
